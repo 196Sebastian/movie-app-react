2a338e945f42754fcc2e84e1ae95767faf624dac
196Sebastian <correa.sebas6@gmail.com>
2023-10-10 17:58:55 +0900

    making our api call

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import * as React from "react";
+import { useEffect } from "react";
+
+// 84e74c9b
+
+const API_URL = "http://www.omdbapi.com/?apikey=84e74c9b";
 
 const App = () => {
+  useEffect(() => {
+    searchMovie("spiderman");
+  }, []);
+
+  const searchMovie = async (title) => {
+    const respone = await fetch(`${API_URL}&s=${title}`);
+    const data = await respone.json();
+    console.log(data.Search);
+  };
+
   return (
     <>
       <h1>Welcome</h1>
